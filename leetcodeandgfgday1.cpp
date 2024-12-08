916bc2a6825d8cf7922e57bfb348ee5c0a00921a
Bhrigu verma <shareesharee73@gmail.com>
2024-12-08 11:46:38 +0530

    ff

---
+++ leetcodeandgfgday1.cpp
@@ -0,0 +1,27 @@
+// User function template for C++
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int getSecondLargest(vector<int> &arr) {
+        // Code Here
+        //get the se   
+        int maxi=0;
+    int n=arr.size();
+    for(int i=0;i<=n-1;i++){
+        //mini ko upgrade karte chalo
+        if(arr[i]>arr[maxi]) 
+        {maxi=i;}
+    }
+    
+    int secondmaxi = -1;
+    for (int i = 0; i < n; i++) {
+            if (arr[i] < arr[maxi]) { // Check for elements smaller than the largest
+                if (secondmaxi == -1 || arr[i] > arr[secondmaxi]) {
+                    secondmaxi = i; // Update index of the second largest
+                }
+            }
+            
+        }return secondmaxi == -1 ? -1 : arr[secondmaxi]; 
+    }
+};
